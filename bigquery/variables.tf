e12b6c5c4299711f7fead02246912094f72a7617
Sreenivasa Reddy <42994002+sreenivasaredy7007@users.noreply.github.com>
2024-11-07 22:16:52 +0530

    Create variables.tf

---
+++ bigquery/variables.tf
@@ -0,0 +1,9 @@
+variable "project_id" {
+  type = string
+  description = "Project ID for Google Cloud resources"
+}
+
+variable "region" {
+  type = string
+  description = "Region for Google Cloud resources"
+}
