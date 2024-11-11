d313782d8083044c2bb97c63d74e332d6e861705
Sreenivasa Reddy <42994002+sreenivasaredy7007@users.noreply.github.com>
2024-11-11 12:35:23 +0530

    Update main.tf
    Terraform template to call pub/sub topic/subscription.

--- main.tf
+++ main.tf
@@ -1,17 +1,46 @@
 provider "google" {
   project = var.project_id
-  region  = var.region
+  region  = "us-central1"
 }
 
-# Include BigQuery resources
-module "bigquery" {
-  source = "./bigquery"
-  project_id = var.project_id
-  region     = var.region
+# Render the topic template
+data "template_file" "pubsub_topic" {
+  template = file("${path.module}/templates/topic.tf.tpl")
+
+  vars = {
+    project_id = var.project_id
+    topic_name = var.topic_name
+  }
 }
 
-# Include Pub/Sub resources
-module "pubsub" {
-  source = "./pubsub"
-  project_id = var.project_id
+# Render the subscription template
+data "template_file" "pubsub_subscription" {
+  template = file("${path.module}/templates/subscription.tf.tpl")
+
+  vars = {
+    project_id        = var.project_id
+    subscription_name = var.subscription_name
+  }
+}
+
+# Output the rendered templates for verification (optional)
+output "rendered_topic" {
+  value = data.template_file.pubsub_topic.rendered
+}
+
+output "rendered_subscription" {
+  value = data.template_file.pubsub_subscription.rendered
+}
+
+# Include resources using Terraform's `resource` blocks for actual deployment
+resource "google_pubsub_topic" "topic" {
+  name    = var.topic_name
+  project = var.project_id
+}
+
+resource "google_pubsub_subscription" "subscription" {
+  name                 = var.subscription_name
+  topic                = google_pubsub_topic.topic.id
+  project              = var.project_id
+  ack_deadline_seconds = 10
 }
